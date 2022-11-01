00713face569d2f95f4824b94ff5fba9db44088f
Lolu <davidebe63@gmail.com>
2022-11-01 18:18:15 +0100

    almost done

--- components/HorizontalScroll.jsx
+++ components/HorizontalScroll.jsx
@@ -1,33 +1,112 @@
+import { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
+import ReactPlayer from 'react-player'
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import ImgVid from './ImgVid';
+import axios from 'axios';
 
-export const HorizontalScroll =()=>{
+
+
+export const HorizontalScroll = ({url})=>{
+    // const[results,setResults]=useState(null)
+    const[cover,setCover]=useState(false)
+    const[results,setResults]=useState([])
+    // const[cover,setCover]=useState(true)
+    console.log(url)
+    console.log('cover:', cover)
+    useEffect(()=>{
+        if(url){
+            axios.get(url).then((response)=>{
+                console.log(response)
+                setResults(response.data)
+                console.log(results)
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+            // const fetchData=async()=>{
+            //     await fetch(url)
+            //     .then(res=>res.json())
+            //     .then(res=>{
+            //         return setResults(res)
+            //     })
+            // }
+            // fetchData()
+        }
+    },[url])
+    
+      console.log(results)
+    
   return (
     <div className='horizontal_scroll_container'>
     <Swiper
-        scrollbar={{
-          hide: true,
-        }}
-        modules={[Scrollbar]}
+        navigation 
+        pagination={true}
+        scrollbar={
+          true
+        }
+        modules={[Scrollbar,Navigation,Pagination]}
+        slidesPerView={3}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        {
+        results && results.map((dt,index)=>{
+        return(
+        <>
+        <div key={index}>
+        <SwiperSlide>
+        <div
+        style={{zIndex:'100000'}}
+        onClick={()=>{
+            setCover(true)
+            console.log('cover:',cover)
+        }}
+        className='swiper_slide'
+        >{
+            dt?.media_type==='image'?(<img
+            onClick={()=>setCover(true)}
+            src={dt.url}
+            alt={dt.title}
+            className='slider_images'
+            />):(<ReactPlayer
+            //className='video'
+            width='30vw'
+            height='30vh'
+            url={dt.url}
+            />)
+        }
+        
+        <p>{dt.title}</p>
+        <p>{dt.copyright}</p>
+        {/* <p>{dt.}</p> */}
+        </div>
+        </SwiperSlide>
+        <div>
+            {cover && 
+            // <div onClick={()=>setCover(false)}>
+                <img
+                src="https://apod.nasa.gov/apod/image/2210/andromeda-over-alps1100.jpg"
+                onClick={()=>setCover(false)}
+                // src='https://api.nasa.gov/planetary/apod?api_key=BkF78ObzKt8S1leoWwMZMjEUqHi4iyLWek81OjBq&start_date=2022-10-22&end_date=2022-10-29&thumbs=true'                               
+                alt='text'
+                className='slider_image_overlay'
+                />
+            // </div>
+            }
+        </div>
+        </div>
+        </>
+        )
+      })
+    }
+    
       </Swiper>
+    
     </div>
   );
 };
--- components/Spotlight.jsx
+++ components/Spotlight.jsx
@@ -2,17 +2,23 @@ import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import ImgVid from './ImgVid'
 // import Latest from './Latest'
+import { RotatingLines } from  'react-loader-spinner'
+
 
 const Spotlight = () => {
   const[data,setData]=useState([])
   const[overlay, setOverlay]=useState(false)
-  
+  const[loading,setLoading]=useState(false)
+
   const fetchData=async()=>{
+    setLoading(true)
     await fetch('https://api.nasa.gov/planetary/apod?api_key=gaff4Pwpu0Qg6woyFty1YhVRxhj4In1ImvOCyFD7&start_date=2022-10-01&end_date=2022-10-29&thumbs=true')
-    .then(res=>res.json())
+    .then(res=>{
+      setLoading(false)
+      return res.json()})
     .then(res=>{
       setData(res)
-      console.log(res)
+      // console.log(res)
     })
   }
   
@@ -26,11 +32,31 @@ const Spotlight = () => {
     <div className='nasa_spotlight_section'>
     <div className='latest_container'>
         <div className='latest_left'>
+        {loading && <RotatingLines
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />}
             <p className='title'>{data[data.length-1]?.title}</p>
             <p className='explanation'>{data[data.length-1]?.explanation.length>900?data[data.length-1].explanation.substring(0,900)+'. . . . . . .':data[data.length-1]?.explanation}</p>
             <p className='copyright'>{data[data.length-1]?.copyright}</p>
         </div>
         <div className='latest_right'>
+        <div
+        style={{marginLeft:'40vw', marginTop:'40px'}}
+        >
+        {loading && <RotatingLines
+        
+        strokeColor="grey"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="96"
+        visible={true}
+      />}
+        </div>
+        
             <img 
             onClick={()=>setOverlay(true)}
             className='latest_image'
